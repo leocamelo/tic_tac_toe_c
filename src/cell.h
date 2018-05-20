8524f462ad9219294ff063df4f39a51bc3cb4598
leocamelo <leonardocamelo.nave@gmail.com>
2018-05-20 12:38:59 -0300

    remove unused cell header

--- src/cell.h
+++
@@ -1,8 +0,0 @@
-#ifndef CELL_H
-#define CELL_H
-
-typedef enum {
-  NIL, X, O
-} Cell;
-
-#endif
--- test/check_board.c
+++ test/check_board.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <check.h>
 #include "check_board.h"
 #include "../src/board.h"
