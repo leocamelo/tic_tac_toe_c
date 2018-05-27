f3802e010b356fa65873ce5c595cc8d0f91104ae
leocamelo <leonardocamelo.nave@gmail.com>
2018-05-27 12:57:52 -0300

    use value over reference on computer moves

--- src/computer.c
+++ src/computer.c
@@ -2,17 +2,17 @@
 #include <time.h>
 #include "computer.h"
 
-static CellPoint computer_center_move(Board *board) {
+static CellPoint computer_center_move(Board board) {
   int center = BOARD_SIZE / 2;
 
-  if (cell_is_empty(board->cells[center][center])) {
+  if (cell_is_empty(board.cells[center][center])) {
     return (CellPoint){center, center};
   }
   return cell_point_null();
 }
 
-static CellPoint computer_random_move(Board *board) {
-  BoardSubset available_cells = board_available_cells(board);
+static CellPoint computer_random_move(Board board) {
+  BoardSubset available_cells = board_available_cells(&board);
   if (available_cells.size == 0) return cell_point_null();
 
   srand(time(NULL));
@@ -23,10 +23,10 @@ static CellPoint computer_random_move(Board *board) {
 }
 
 CellPoint computer_move(Board board) {
-  CellPoint center_move = computer_center_move(&board);
+  CellPoint center_move = computer_center_move(board);
   if (!cell_point_is_null(&center_move)) return center_move;
 
-  CellPoint random_move = computer_random_move(&board);
+  CellPoint random_move = computer_random_move(board);
   if (!cell_point_is_null(&random_move)) return random_move;
 
   return cell_point_null();
