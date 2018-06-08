d33afcef9c5c5a840ce4a3c6c198be5a926f6a38
Leonardo Camelo <camelo@pluga.co>
2018-06-08 11:35:12 -0300

    simplify human move validation

--- src/human.c
+++ src/human.c
@@ -3,20 +3,6 @@
 #include <math.h>
 #include "human.h"
 
-static int human_cell_point_is_available(Board *board, CellPoint *point) {
-  BoardSubset subset = board_available_cells(board);
-
-  int i;
-  for (i = 0; i < subset.size; i++) {
-    if (subset.points[i].x == point->x && subset.points[i].y == point->y) {
-      board_subset_free(&subset);
-      return 1;
-    }
-  }
-  board_subset_free(&subset);
-  return 0;
-}
-
 CellPoint human_cell_point_by_index(int index) {
   int aux = 1, i, j;
 
@@ -41,7 +27,8 @@ CellPoint human_move(Board *board) {
   if (input > 0 && input <= limit) {
     CellPoint point = human_cell_point_by_index(input);
 
-    if (!cell_point_is_null(&point) && human_cell_point_is_available(board, &point)) {
+    if (!cell_point_is_null(&point) &&
+        cell_is_empty(board->cells[point.x][point.y])) {
       return point;
     }
   }
