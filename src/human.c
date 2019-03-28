9cefefe571e28bee49d2bbf62ecb0af0d6bd37a1
leocamelo <leonardocamelo.nave@gmail.com>
2019-03-28 16:31:03 -0300

    adjust human player loop

--- src/human.c
+++ src/human.c
@@ -16,21 +16,24 @@ CellPoint *human_cell_point_by_index(int index) {
 }
 
 CellPoint *human_move(Board *board) {
-  int input, limit = pow(BOARD_SIZE, 2);
+  int limit = pow(BOARD_SIZE, 2), input;
 
   char *grid = grid_from_board(board);
+
+  CellPoint *point;
+
   PRINTF_FN("%s\n", grid);
   free(grid);
 
-  PRINTF_FN("Enter [1-%d] to mark or 0 to quit: ", limit);
-  if (!SCANF_FN("%d", &input) || !input) return NULL;
+  while (1) {
+    PRINTF_FN("Enter [1-%d] to mark or 0 to quit: ", limit);
+    if (!SCANF_FN("%d", &input) || !input) return NULL;
 
-  if (input > 0 && input <= limit) {
-    CellPoint *point = human_cell_point_by_index(input);
-
-    if (point && cell_is_empty(board->cells[point->x][point->y])) {
-      return point;
+    if (input > 0 && input <= limit) {
+      point = human_cell_point_by_index(input);
+      if (point && cell_is_empty(board->cells[point->x][point->y])) break;
     }
   }
-  return human_move(board);
+
+  return point;
 }
